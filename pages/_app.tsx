cf6b13525c784d431a07132b6935d63e18a027f5
amith2368 <amith2368@gmail.com>
2024-08-16 20:11:24 +0530

    added adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,32 +19,33 @@ config.autoAddCss = false;
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
       <Head>
-            <title>KNAnime</title>
-            <meta property="og:title" content="Anime for the normies and otakus" key="title" />
-            {/* Favicon for general browsers */}
-            <link rel="icon" href="/favicon.ico" />
+          <title>KNAnime</title>
+          <meta name="google-adsense-account" content="ca-pub-1497909257443794"/>
+          <meta property="og:title" content="Anime for the normies and otakus" key="title"/>
+          {/* Favicon for general browsers */}
+          <link rel="icon" href="/favicon.ico"/>
 
-            {/* Apple Touch Icon */}
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          {/* Apple Touch Icon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
 
-            {/* Android Chrome Icon */}
-            <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-            <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+          {/* Android Chrome Icon */}
+          <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png"/>
+          <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png"/>
 
-            {/* Microsoft Tiles */}
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+          {/* Microsoft Tiles */}
+          <meta name="msapplication-TileColor" content="#ffffff"/>
+          <meta name="msapplication-TileImage" content="/mstile-144x144.png"/>
 
-            {/* Favicon for different sizes */}
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          {/* Favicon for different sizes */}
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 
-            {/* Manifest file */}
-            <link rel="manifest" href="/site.webmanifest" />
-        </Head>
-        <MantineProvider theme={theme}>
-        <Layout>
-            <Component {...pageProps} />
+          {/* Manifest file */}
+          <link rel="manifest" href="/site.webmanifest"/>
+      </Head>
+      <MantineProvider theme={theme}>
+          <Layout>
+          <Component {...pageProps} />
             <Analytics />
         </Layout>
         </MantineProvider>
