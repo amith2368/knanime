0cf7154650592c3c7291d2d2eeaa286411cce384
amith2368 <amith2368@gmail.com>
2024-08-19 00:05:48 +0530

    New PWA changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,8 +20,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (<>
       <Head>
           <title>KNAnime</title>
+          <meta name="viewport" content="width=device-width,initial-scale=1"/>
+          <meta name="description" content="KNANime PWA App"/>
+          <link rel="shortcut icon" href="/favicon.ico"/>
+          <link rel="mask-icon" href="/favicon.ico" color="#FFFFFF"/>
+          <meta name="theme-color" content="#ffffff"/>
           <meta name="google-adsense-account" content="ca-pub-1497909257443794"/>
-          <meta property="og:title" content="Anime for the normies and otakus" key="title"/>
           {/* Favicon for general browsers */}
           <link rel="icon" href="/favicon.ico"/>
 
@@ -40,15 +44,45 @@ export default function App({ Component, pageProps }: AppProps) {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 
+          {/*PWA Related*/}
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+          <link
+              rel="apple-touch-icon"
+              sizes="152x152"
+              href="/apple-touch-icon.png"
+          />
+          <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+          />
+          <link
+              rel="apple-touch-icon"
+              sizes="167x167"
+              href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json"/>
+          <meta name="twitter:card" content="summary"/>
+          <meta name="twitter:title" content="KNAnime"/>
+          <meta name="twitter:url" content="https://knanime.netlify.com"/>
+          <meta name="twitter:description" content="KNAnime PWA for Anroid Support"/>
+          <meta name="twitter:image" content="/logo.png"/>
+          <meta name="twitter:creator" content="@velvetvine"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="KNAnime"/>
+          <meta property="og:description" content="KNAnime PWA for Anroid Support!"/>
+          <meta property="og:site_name" content="KNAnime"/>
+          <meta property="og:url" content="https://knanime.netlify.com"/>
+          <meta property="og:image" content="/logo.png"/>
           {/* Manifest file */}
-          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="manifest" href="/manifest.json"/>
       </Head>
       <MantineProvider theme={theme}>
           <Layout>
-              <Component {...pageProps} />
-              <Analytics />
-        </Layout>
-        </MantineProvider>
+          <Component {...pageProps} />
+              <Analytics/>
+          </Layout>
+      </MantineProvider>
 
-    </>);
+  </>);
 }
